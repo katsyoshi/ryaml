94245801bf59615370a39c306593cd2676d8965d
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2025-06-28 13:51:53 +0900

    call benchmark script from rake task

--- Rakefile
+++ Rakefile
@@ -11,19 +11,7 @@ end
 
 desc "Run benchmarks"
 task :bench do
-  require "benchmark"
-  require "ryaml"
-  require "yaml"
-
-  str = "YAML: 42\nJSON: six".freeze
-  N = 100_000
-  ryaml = Ryaml::Parser.new(str)
-  yaml = YAML.parse(str)
-
-  Benchmark.bm do |x|
-    x.report("test file stdlib") { N.times { yaml.to_ruby } }
-    x.report("test file ryaml") { N.times { ryaml.parse; ryaml.lines_enum.rewind } }
-  end
+  require "./benchmark/bm.rb"
 end
 
 task default: :test
--- benchmark/bm.rb
+++ benchmark/bm.rb
@@ -1,13 +1,42 @@
 require "benchmark"
+require "benchmark/ips"
 require "ryaml"
 require "yaml"
 
-N = 10000
+N = 10_000
 yaml_string = File.read("./samples/mapping.yml")
 ryaml = Ryaml::Parser.new(yaml_string)
 
+puts "YJIT DISABLED"
 Benchmark.bm do |x|
-  x.report("YAML parse:") { N.times { YAML.load(yaml_string) } }
+  x.report("YAML#.load:") { N.times { YAML.load(yaml_string) } }
   x.report("RYAML::Parser#.parse:") { N.times { Ryaml::Parser.parse(yaml_string) } }
-  x.report("RYAML::Parser.parse:") { N.times { ryaml.parse; ryaml.lines_enum.rewind } }
+  x.report("RYAML::Parser#parse!:") { N.times { ryaml.parse! } }
+  x.report("RYAML::Parser#parse:") { N.times { ryaml.parse; ryaml.rewind } }
+  return unless defined?(RubyVM::YJIT)
+end
+
+Benchmark.ips do |x|
+  x.report("YAML#.load:") { YAML.load(yaml_string) }
+  x.report("RYAML::Parser#.parse:") { Ryaml::Parser.parse(yaml_string) }
+  x.report("RYAML::Parser#parse!:") { ryaml.parse! }
+  x.report("RYAML::Parser#parse:") { ryaml.parse; ryaml.rewind }
+end
+
+exit unless defined? RubyVM::YJIT
+puts "YJIT ENABLED"
+
+RubyVM::YJIT.enable
+Benchmark.bm do |x|
+  x.report("YAML#.load:") { N.times { YAML.load(yaml_string) } }
+  x.report("RYAML::Parser#.parse:") { N.times { Ryaml::Parser.parse(yaml_string) } }
+  x.report("RYAML::Parser#parse!:") { N.times { ryaml.parse! } }
+  x.report("RYAML::Parser#parse:") { N.times { ryaml.parse; ryaml.rewind } }
+end
+
+Benchmark.ips do |x|
+  x.report("YAML#.load:") { YAML.load(yaml_string) }
+  x.report("RYAML::Parser#.parse:") { Ryaml::Parser.parse(yaml_string) }
+  x.report("RYAML::Parser#parse!:") { ryaml.parse! }
+  x.report("RYAML::Parser#parse:") { ryaml.parse; ryaml.rewind }
 end
